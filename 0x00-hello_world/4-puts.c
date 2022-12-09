efd3be87d0809189e974c03e5149285c6da7cc3d
kamalfola <kamalfolarin@folarin.com>
2022-12-08 17:56:47 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  * main- main block
- * return: 0
+ * return: o
  */
-int main (void)
+int main(void)
 {
 puts ("\"Programming is like building a multilingual puzzle");
 return (0);
