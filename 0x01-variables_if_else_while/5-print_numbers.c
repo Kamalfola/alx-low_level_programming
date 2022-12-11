559186585533bd78665f5a34d08d61e5192bae46
kamalfola <kamalfolarin@folarin.com>
2022-12-11 13:54:45 -0800

    print base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * description - print numbers in base 10
+ * return - 0
+ */
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+printf("%d\n", i);
+i++;
+}
+return (0);
+}
