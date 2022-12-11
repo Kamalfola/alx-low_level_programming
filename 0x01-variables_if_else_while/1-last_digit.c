8e88d7247f4f2417629776ffad120adb06610950
kamalfola <kamalfolarin@folarin.com>
2022-12-11 09:23:36 -0800

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 /**
  * main - Entry point
  * Description: prinf if n is greater than 5 or less than 6
- * Return: A;ways 0 (success)
+ * Return: Always 0 (success)
  */
 int main (void)
 {
@@ -15,7 +15,7 @@ if (n > 5)
 {
 printf("%d is greater than 5\n", n);
 }
-else if (n < 6 > 0)
+else if (n < 6)
 {
 printf("%d is less than 6 and not 0\n", n);
 }
