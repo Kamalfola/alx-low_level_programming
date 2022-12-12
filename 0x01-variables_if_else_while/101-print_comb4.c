fc142ff0a1841340e00ad3f4de5e4f77bcf9910f
kamalfola <kamalfolarin@folarin.com>
2022-12-11 17:26:43 -0800

    print combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description - print different combonations of three digits
+ * Return: 0
+ */
+int main(void)
+{
+int a;
+int b;
+int c;
+for (a = 48; a < 58; a++)
+{
+for (b = 48; b < 58; b++)
+{
+if (b > 1)
+{
+for (c = 48; c < 58; c++)
+{
+if ( c > b)
+{
+putchar(a);
+putchar(b);
+putchar(c);
+if (a == 55 && b == 56 && c == 57)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
