2c2291a4ecefc03bf3cb5bbdfe927e38f49c5c74
kamalfola <kamalfolarin@folarin.com>
2022-12-13 15:36:16 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 /**
  * main - Entry point
  * Description - Printing putchar
  * Return: 0
  */
-void put()
+int main(void)
 {
-printf("_putchar");
-}
-int main()
-{
-put();
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
