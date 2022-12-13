c42a31fdab19c8adf8a84981a84b85ebd8025efa
kamalfola <kamalfolarin@folarin.com>
2022-12-13 10:36:40 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description - Printing putchar
+ * Return: 0
+ */
+void put()
+{
+printf("_putchar");
+}
+int main()
+{
+put();
+return (0);
+}
