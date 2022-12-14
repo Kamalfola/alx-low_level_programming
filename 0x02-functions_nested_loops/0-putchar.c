31f4d69dda8fbc1805cc2704c6b4c4898289f4e5
kamalfola <kamalfolarin@folarin.com>
2022-12-14 07:20:25 -0800

    put char

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,17 +4,18 @@
  * Description - Printing putchar
  * Return: 0
  */
-int _putchar(char c)
+
+int main(void);
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 return (0);
 }
