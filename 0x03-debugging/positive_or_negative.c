abaf4816487cdcdea06b062cb942cb8047a28b0c
kamalfola <kamalfolarin@folarin.com>
2022-12-15 10:28:57 -0800

    positive debugging

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,23 +1,24 @@
-#include "main.h"
-#include <stdio.h>                                                             
-#include <stdlib.h>                                            
-#include <time.h>                                                                                                               
+#include "main.h"                                     
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * main - entry point                                                                                                           
- * Description: print for positive or negative numbers                                                                          
- * Return: Always 0 (succes)                                                                                                    
- */                                                                                                                             
-int main(void)                                                                                                                  
-{                                                                                                                               
-int n;                                                                                                                          
-srand(time(0));                                                                                                                 
-n = rand() - RAND_MAX / 2;                                                                                                      
-if (n > 0)                                                                                                                      
-{                                                                                                                               
-printf("%d is positive\n", n);                                                                                                  
-}                                                                                                                               
-else if (n < 0)                                                                                                                 
-{                                                                                                                               
-printf("%d is negative\n", n);                                                                                                  }
+ * main - entry point
+ * Description: print for positive or negative numbers
+ * Return: Always 0 (succes)
+ */
+int main(void)
+{
+int i;
+srand(time(0));
+i = rand() - RAND_MAX /2;
+if (i > 0)
+{
+printf("%d is positive\n", i);
+}
+else if (i < 0)
+{
+printf("%d is negative\n", i);
+}
 return (0);
 }
