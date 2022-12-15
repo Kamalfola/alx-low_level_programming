92bfa67e80fb869eae4a9ac0d5b121a4d0c59b51
kamalfola <kamalfolarin@folarin.com>
2022-12-15 12:37:12 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - printing numbers
+ * Description - print numbers from 0-9
+ * Return: 0
+ */
+void print_numbers(void)
+{
+int c;
+if (c >= 0 && c <= 9)
+{
+_putchar(c);
+}
+_putchar('\n')
+}
+
