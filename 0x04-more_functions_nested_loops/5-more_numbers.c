9088c8c918c51f9d963ac6aa9adbb6b7bff60c9b
kamalfola <kamalfolarin@folarin.com>
2022-12-15 15:00:46 -0800

    more

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * more_numbers - print more numbers
+ * Return: 0
+ */
+void more_numbers(void)
+{
+int i, j;
+for (j = 1; j <= 10; j++)
+{
+for (i = 0; i <= 14; i++)
+{
+_putchar(i);
+}
+_putchar(j);
+}
+_putchar('\n');
+}
