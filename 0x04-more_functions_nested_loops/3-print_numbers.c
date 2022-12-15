cc5437841484524cb4829067969754359a06bf32
kamalfola <kamalfolarin@folarin.com>
2022-12-15 13:57:41 -0800

    print

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
  * print_numbers - printing numbers
+ *
  * Return: 0
  */
 void print_numbers(void)
 {
 char c;
-for (c = '0'; c < '10'; c++)
+for (c = '0'; c <= '9'; c++)
 {
 _putchar(c);
 }
