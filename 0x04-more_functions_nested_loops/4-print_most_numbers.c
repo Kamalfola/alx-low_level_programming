6f4027d37b01e9dce593431ea1c8009885e5268f
kamalfola <kamalfolarin@folarin.com>
2022-12-15 13:27:57 -0800

    most

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers except 2 and 4
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+for (i = 0; i < 10; i++)
+{
+if (i == 2 && i == 4)
+{continue;
+}
+_putchar(i);
+}
+}
