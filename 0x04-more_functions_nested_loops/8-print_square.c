b850ccc0494b17854dfbd926a88d92f7bb8f303c
kamalfola <kamalfolarin@folarin.com>
2022-12-16 15:13:44 -0800

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+nclude "main.h"
+/**
+ * print_square - definition
+ * @size: the length and width
+ * Return: void
+ */
+void print_square(int size)
+{
+int c, r;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (c = 1; c <= size; c++)
+{
+_putchar('#');
+for (r = 2; r <= size; r++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
