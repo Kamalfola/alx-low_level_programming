817d1fbc608a943e9869f0438ee7c19cc340fba5
kamalfola <kamalfolarin@folarin.com>
2022-12-16 15:34:40 -0800

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * 3 prints fizz
+ * 5 prints buzz
+ * 3 & 5 prints fizz-buzz
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+char f = "Fizz";
+char b = "Buzz";
+char fb = "FuzzBuzz";
+for ( i = 0; i <= 100; i++)
+{
+if (i == 100)
+{
+printf("%s", b);
+}
+else if (i % 3 == 0)
+{
+printf("%s", f);
+}
+else if (i % 5)
+{
+printf("%s", b);
+}
+else if (i % 3 == 0) && (i % 5 == 0)
+{
+printf("%s", fb);
+}
+else 
+{
+printf(i);
+}
+printf('\n');
+return (0);
+}
+}
