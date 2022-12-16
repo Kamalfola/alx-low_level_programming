011f796c6546af4fbb177f40409591797a644913
kamalfola <kamalfolarin@folarin.com>
2022-12-16 14:25:14 -0800

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_diagonal - entry point
+ * @n: number of times
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
