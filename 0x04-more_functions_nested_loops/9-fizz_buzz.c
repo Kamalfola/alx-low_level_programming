a5c2bfa1685e8255995afb56f12666a9be73a336
kamalfola <kamalfolarin@folarin.com>
2022-12-16 15:45:59 -0800

    fuzzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,32 +10,23 @@
 int main(void)
 {
 int i;
-char f = "Fizz";
-char b = "Buzz";
-char fb = "FuzzBuzz";
+char f[] = "Fizz";
+char b[] = "Buzz";
+char fb[] = "FuzzBuzz";
 for (i = 0; i <= 100; i++)
 {
 if (i == 100)
-{
 printf("%s", b);
-}
 else if (i % 3 == 0)
-{
 printf("%s", f);
-}
 else if (i % 5)
-{
 printf("%s", b);
-}
 else if ((i % 3 == 0) && (i % 5 == 0))
-{
 printf("%s", fb);
-}
 else
-{
-printf(i);
+printf("%d", i);
 }
-printf('\n');
+printf("\n");
 return (0);
 }
-}
+
