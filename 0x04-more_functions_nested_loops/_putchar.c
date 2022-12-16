88b04916e9f0ec08550d5eb22832a50ed87ac4b2
kamalfola <kamalfolarin@folarin.com>
2022-12-16 13:52:07 -0800

    fix

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,11 +1,12 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
- * main - check the code
- * Return: Always 0.
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int main(void)
+int _putchar(char c)
 {
-print_most_numbers();
-return (0);
+return (write(1, &c, 1));
 }
