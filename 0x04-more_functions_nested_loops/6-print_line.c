1425ebd6f334f8e4f3613125a2572f88a949cb10
kamalfola <kamalfolarin@folarin.com>
2022-12-16 14:14:18 -0800

    print

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,4 +1,14 @@
 #include "main.h"
 /**
  * print_line - print line
- * @n: 
+ * Return: void
+ */
+void print_line(int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
