6af4d743f9070bf4f9f3df48a9f47338f12287b6
kamalfola <kamalfolarin@folarin.com>
2022-12-16 16:24:16 -0800

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "math.h"
+/**
+ * print_triangle - prints a triangle
+ * @size: size of the triangle
+ * return: void
+ */
+void print_triangle(int size)
+{
+int l, b, h;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (l = 0; l <= (size - 1); l++)
+{
+for (b = 0; b <= (size - 1) - l; b++)
+{
+_putchar(' ');
+}
+for (h = 0; h <= l; h++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
