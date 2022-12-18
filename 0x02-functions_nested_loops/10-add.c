6d904568709a7c660abea26f561455c9394b38e6
kamalfola <kamalfolarin@folarin.com>
2022-12-17 21:00:54 -0800

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - add two numbers and print the result
+ * @a: first parameter
+ * @b: second parameter
+ * Return: addition
+ */
+int add(int a, int b)
+{
+return(a + b);
+}
