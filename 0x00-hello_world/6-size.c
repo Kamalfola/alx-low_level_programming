d87b643f62fc7a671afbb912adc27491e6a84795
kamalfola <kamalfolarin@folarin.com>
2022-12-19 15:36:58 -0800

    len

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+int i;
+char c;
+double d;
+
+printf("size of int is: \n", sizeof (i));
+printf("size of char is: \n", sizeof (c));
+printf("size of double i \n", sizeof (d));
+return (0);
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * _strlen - entry point
+ * @s: the string
+ * return: 0
+ */
+int_strlen(char* s)
+{
+strlen(s);
+return (0);
+}
