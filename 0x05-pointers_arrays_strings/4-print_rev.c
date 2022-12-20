33a6f8eb6f87a1a6163445bb84a435ac2abe5446
kamalfola <kamalfolarin@folarin.com>
2022-12-20 15:20:03 -0800

    rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * print_rev - enntry point
- * @: prints in reverse
+ * @s: prints in reverse
  */
- void print_rev(char *s)
+void print_rev(char *s)
 {
 int i = 0;
 while (s[i] != '\0')
 i++;
-for (i = i - 1; i > 0; i--)
+for (i = i - 1; i >= 0; i--)
 _putchar(s[i]);
 _putchar('\n');
 }
