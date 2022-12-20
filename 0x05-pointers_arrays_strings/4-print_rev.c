322adf213ab331086e8789d5bf2e91568ab07df5
kamalfola <kamalfolarin@folarin.com>
2022-12-20 15:04:31 -0800

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_rev - enntry point
+ * @: prints in reverse
+ */
+ void print_rev(char *s)
+{
+int i - 0;
+while (s[i] != '\0')
+i++;
+for (i = i - 1; i > 0; i--)
+_putchar(s[i]);
+_putchar('\n');
+}
