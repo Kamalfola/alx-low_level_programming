8da75fb2d6a9721216d7a08a37a8a4d33b4150f2
kamalfola <kamalfolarin@folarin.com>
2022-12-20 14:40:31 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - the strings entry point
+ * @str: the string to be printed
+ * Return: void
+ */
+void _puts(char *str)
+{
+while (*str != 0)
+{
+_putchar(*str++);
+_putchar('\n');
+}
