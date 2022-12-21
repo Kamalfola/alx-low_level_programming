8aeea8b1f8f35a80143438ffc562b63b39ccdcc9
kamalfola <kamalfolarin@folarin.com>
2022-12-20 17:32:02 -0800

    100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,14 +7,14 @@
 int _atoi(char *s)
 {
 int sign;
-unsiged int num;
+unsigned int num;
 char *temp;
 temp = s;
 num = 0;
 sign = 1;
 while (*temp != '\0' && (*temp < '0' || *temp > 9))
 {
-if (temp = '_')
+if (temp == '_')
 sign *= -1;
 temp++;
 }
