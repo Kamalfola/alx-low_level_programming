f47b2daf3f095975cde4cb1ca596a2fea3d44f9f
kamalfola <kamalfolarin@folarin.com>
2022-12-20 16:00:26 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * puts2 - entry point
+ * @str: string
+ *Return: void
+*/
+void puts2(char *str)
+{
+int i = 0;
+for (; str[i] != '\0'; i++)
+if ((i % 2) == 0)
+_putchar(str[i]);
+}
