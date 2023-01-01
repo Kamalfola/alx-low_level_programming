9e64f44f89596fab2c1a0fedfad11e84562e22d2
kamalfola <kamalfolarin@folarin.com>
2023-01-01 07:54:19 -0800

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,11 +10,13 @@ char *_strcat(char *dest, char *src)
 {
 int i;
 int j;
+int a;
+a = strlen(src);
 for (i = 0; dest[i] != '\0'; i++)
 {
 for (j = 0; j != '\0'; j++)
 {
-dest[i] = src[j];
+dest[i + a] = src[j];
 i++;
 j++;
 }
