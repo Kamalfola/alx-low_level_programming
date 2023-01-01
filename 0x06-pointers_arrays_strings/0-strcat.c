6ecf0ff1c02acd2515eb50ae6d8eee8b745e634b
kamalfola <kamalfolarin@folarin.com>
2023-01-01 07:49:16 -0800

    0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,15 +1,24 @@
-#include <main.h>
+#include "main.h"
 #include <string.h>
 /**
  * _strcat - entry point
- * @dest - destination string
- * @src - source string
+ * @dest: destination string
+ * @src: source string
  * Return: destination
  */
 char *_strcat(char *dest, char *src)
 {
-char dest[];
-char src[];
-strcat(dest, src);
-return (dest);
+int i;
+int j;
+for (i = 0; dest[i] != '\0'; i++)
+{
+for (j = 0; j != '\0'; j++)
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+}
+dest[i] = '\0';
+return (0);
 }
