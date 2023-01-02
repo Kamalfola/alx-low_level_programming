8a6d1744bd755ce599915f5494d944f14878d83a
kamalfola <kamalfolarin@folarin.com>
2023-01-01 17:37:46 -0800

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strncat - entry point
+ * @dest: destination
+ * @src: source string
+ * @n: number if strings
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i, j, n;
+i =  0;
+while (i != '\0')
+{
+i++;
+}
+j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+return (dest);
+}
+
