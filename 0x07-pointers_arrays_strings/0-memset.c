a9a87da3c133c5d4af213a7a190d75d82c7066ad
kamalfola <kamalfolarin@folarin.com>
2023-01-02 14:34:43 -0800

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _memset - entry point
+ * @s: pointer to the memory area
+ * @n: number of bytes
+ * @b: constant byte
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+int i = 0;
+for (; n > 0; i++)
+{
+s[i] = b;
+n--;
+}
+return (s);
+}
