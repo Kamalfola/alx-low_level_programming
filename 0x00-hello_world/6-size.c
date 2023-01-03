47a1065f0ee3181176cde4288e732a9a859ae4dc
kamalfola <kamalfolarin@folarin.com>
2023-01-03 12:43:56 -0800

    corrections

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
 int main(void)
 {
 int i;
 char c;
 double d;
 
-printf("size of int is: \n", sizeof (i));
-printf("size of char is: \n", sizeof (c));
-printf("size of double i \n", sizeof (d));
+printf ("size of int is: \n", sizeof(i));
+printf ("size of char is: \n", sizeof(c));
+printf ("size of double i \n", sizeof(d));
 return (0);
 }
