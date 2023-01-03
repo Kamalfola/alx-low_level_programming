a396aae81f22f0b96c2dfbc80b60ad160d7f87b5
kamalfola <kamalfolarin@folarin.com>
2023-01-03 13:10:30 -0800

    chess

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,12 +11,11 @@ char *_strstr(char *haystack, char *needle)
 	{
 		char *a = haystack;
 		char *b = needle;
-		while (*a  ==  *b  &&  *b  != '\0')
-
-		{
+	while (*a == *b  &&  *b != '\0')
+	{
 			a++;
 			b++;
-		}
+	}
 		if (*b == '\0')
 			return (haystack);
 	}
