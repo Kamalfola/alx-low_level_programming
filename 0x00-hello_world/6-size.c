c5b8ea615fa9763a04e5c832b637161207cf4d49
kamalfola <kamalfolarin@folarin.com>
2023-01-03 12:48:58 -0800

    corrections

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,9 +8,8 @@ int main(void)
 int i;
 char c;
 double d;
-
-printf ("size of int is: \n", sizeof(i));
-printf ("size of char is: \n", sizeof(c));
-printf ("size of double i \n", sizeof(d));
+printf ("size of int is: \n", sizeof (int));
+printf ("size of char is: \n", sizeof (char));
+printf ("size of double i \n", sizeof (double));
 return (0);
 }
