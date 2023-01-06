906d67d21d65bd91c4678b12c0f5682800115d37
kamalfola <kamalfolarin@folarin.com>
2023-01-06 14:25:37 -0800

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - multiply two numbers
+ * @argc: argument count
+ * @argv: argument string
+ * Return: 1
+ */
+int main(int argc, char *argv[])
+{
+	int i, muls = 0;
+
+	if
+	(argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			muls = atoi(argv[1]) * atoi(argv[2]);
+		}
+			printf("%d\n", muls);
+	}
+		else
+			printf("ERROR\n");
+	return (1);
+}
