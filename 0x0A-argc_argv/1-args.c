f7a4697b003303e075e3680cf76d96e9fa6f9a82
kamalfola <kamalfolarin@folarin.com>
2023-01-06 10:08:52 -0800

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - printing the number of an argument
+ * @argc - numbers 
+ * @argv - strings
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+(void)*argv;
+printf("%d\n", argc);
+return (0);
+}
