1964f149e7a015f516a0f4f6ca014ab62ef407d3
kamalfola <kamalfolarin@folarin.com>
2023-01-10 04:47:27 -0800

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,17 +13,15 @@ char *_strncat(char *dest, char *src, int n)
 int i;
 int len;
 len = strlen(dest);
-for (i = 0; i < n; i++)
+for (i = 0; i < n && src[i] != '\0'; i++)
 {
 	dest[len + i] = src[i];
 }
-for (; i <n; i++)
-{
-	dest[i] = '\0';
-}
+	dest[len + i] = '\0';
 return (dest);
 }
-/*i =  0;
+/*
+ * i =  0;
 while (i != '\0')
 {
 i++;
@@ -37,6 +35,7 @@ j++;
 }
 dest[i] = '\0';
 return (dest);
-}*/
+}
+*/
 
 
