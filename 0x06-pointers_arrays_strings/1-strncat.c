86d857c6bc373a70f8f1ed34c08c3e7f362ff163
kamalfola <kamalfolarin@folarin.com>
2023-01-10 04:31:56 -0800

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 /**
  * _strncat - entry point
  * @dest: destination
@@ -10,8 +11,19 @@
 char *_strncat(char *dest, char *src, int n)
 {
 int i;
-int j;
-i =  0;
+int len;
+len = strlen(dest);
+for (i = 0; i < n; i++)
+{
+	dest[len + i] = src[i];
+}
+for (; i <n; i++)
+{
+	dest[i] = '\0';
+}
+return (dest);
+}
+/*i =  0;
 while (i != '\0')
 {
 i++;
@@ -25,5 +37,6 @@ j++;
 }
 dest[i] = '\0';
 return (dest);
-}
+}*/
+
 
