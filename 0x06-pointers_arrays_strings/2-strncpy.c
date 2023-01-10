ade175ba1408fd54b63bfe89bf137de47c865fd5
kamalfola <kamalfolarin@folarin.com>
2023-01-10 03:59:15 -0800

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncpy - copyn numbers of a string
+ * @n: nunmber of string
+ * @dest: destination
+ * @src: source
+ * Return: 0
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, len;
+	len = strlen(dest);
+	for (i = 0; i < n; i++)
+	{
+		dest[len + i] = src[i];
+	}
+		dest[i] = '\0';
+return (dest);
+}
