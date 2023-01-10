1976f318e982ab99108242fd96f1e7f8f16a82e2
kamalfola <kamalfolarin@folarin.com>
2023-01-10 04:09:47 -0800

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,13 +9,14 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, len;
-
-	len = strlen(dest);
+	int i;
 	for (i = 0; i < n; i++)
 	{
-		dest[len + i] = src[i];
+		dest[i] = src[i];
 	}
+	for (; i < n; i++)
+	{
 		dest[i] = '\0';
+	}
 return (dest);
 }
