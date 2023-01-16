adfb34ba13ba1a2434b335cf0110432cf24ce11f
kamalfola <kamalfolarin@folarin.com>
2023-01-16 13:53:05 -0800

    dogh

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct - structure
+ * struct dog - structure
+ * @owner: dog owner
+ * @name: nameof dog
+ * @age: age of the dog
  */
-struct dog {
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
-};
+}
 typedef struct dog doggy;
 #endif
 
