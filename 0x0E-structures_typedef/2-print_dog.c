98fa23f9816041695e3582a2c4aae953d6a34a52
kamalfola <kamalfolarin@folarin.com>
2023-01-16 16:02:03 -0800

    print_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,7 @@ void print_dog(struct dog *d)
 		return;
 	if (d->name == NULL)
 	{
-		printf("Name: nil\n");
+		printf("Name: (nil)\n");
 	}
 	else
 	{
@@ -19,7 +19,7 @@ void print_dog(struct dog *d)
 	}
 	if ((*d).age < 0)
 	{
-		printf("Age: nil\n");
+		printf("Age: (nil)\n");
 	}
 	else
 	{
@@ -27,7 +27,7 @@ void print_dog(struct dog *d)
 	}
 	if ((*d).owner == NULL)
 	{
-		printf("Owner: (nil)");
+		printf("Owner: (nil)\n");
 	}
 	else
 	{
