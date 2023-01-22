c91a1ebafc722bc627c1b5c1641dc3088faa882f
kamalfola <kamalfolarin@folarin.com>
2023-01-21 16:45:17 -0800

    print

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,7 +14,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *p;
 
-	separator = ", ";
+	/*separator = ", ";*/
 	va_start(arg, n);
 	for (i = 0; i < n; i++)
 	{
