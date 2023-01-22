128ec34e3a68c2d34b751323b88a96f88ce289d8
kamalfola <kamalfolarin@folarin.com>
2023-01-21 16:51:50 -0800

    print

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		printf("%s", p);
 		}
 		else
-			printf("nil");
+			printf("(nil)");
 		if (i < n - 1)
 		{
 			if (separator)
