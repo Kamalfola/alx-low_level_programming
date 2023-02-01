246ce719524d349f7ee1d75aeb6633e2d323c2aa
Kamal <kamalfolarin@gmail.com>
2023-01-31 18:53:37 -0800

    linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,13 +12,11 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 		unsigned int node;
-
-			for (node = 0; node < index; node++)
-					{
+for (node = 0; node < index; node++)
+{
 if (head == NULL)
-										return (NULL);
-
-										head = head->next;
+	return (NULL);
+head = head->next;
 										}
 return (head);
 }
