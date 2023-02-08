a8a3c9416a12f876845c5e079e806ecc073edabf
Kamal <kamalfolarin@gmail.com>
2023-02-08 08:12:41 -0800

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		o = open(filename, O_WRONLY | O_EXCL, "a");
+		o = open(filename, O_WRONLY | O_EXCL, O_APPEND);
 		w = write(o, text_content, n);
 	}
 	if (filename == NULL)
