51c04b88ecd1fd6e0ae346b065cd3fbf2586200e
Kamal <kamalfolarin@gmail.com>
2023-02-08 07:14:20 -0800

    create_file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_file - creating files
+ * @filename: The name of the file to be created
+ * @text_content: content of the file
+ * Return: 1 or -1 if error
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int o, w;
+	int n = 0;
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	w = write(o, text_content, n);
+	if ((o == -1) | (w == -1))
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		for (n = 0; text_content[n];)
+			n++;
+	}
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	close(o);
+	return (1);
+}
