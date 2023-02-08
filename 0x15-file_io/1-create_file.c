40f964f40d9659c79e7867e4217ae479103f2703
Kamal <kamalfolarin@gmail.com>
2023-02-08 09:21:06 -0800

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,17 +26,6 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	/*
-	if (text_content != NULL)
-	{
-		for (n = 0; text_content[n];)
-			n++;
-	}
-	if (filename == NULL)
-	{
-		return (-1);
-	}
-	*/
 	close(o);
 	return (1);
 }
