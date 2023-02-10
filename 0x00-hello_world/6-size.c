c895ee06f68ea4eeaccc62c90b8f3ac219d8950f
Kamal <kamalfolarin@gmail.com>
2023-02-10 15:36:44 -0800

    hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,12 @@
  */
 int main(void)
 {
-int i;
-char c;
-double d;
-printf ("size of int is: \n", sizeof (int));
-printf ("size of char is: \n", sizeof (char));
-printf ("size of double i \n", sizeof (double));
+
+printf("size of int: %d byte(s)\n",(int) sizeof (char));
+printf("size of char: %d byte(s)\n", (int) sizeof (int));
+printf("size of long int: %d byte(s)\n", (int) sizeof (long int));
+printf("size of long long int: %d byte(s)\n", (int) sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
+
 return (0);
 }
