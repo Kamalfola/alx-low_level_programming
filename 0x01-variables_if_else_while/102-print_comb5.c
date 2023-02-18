5d0df90e6b2ef467733c04846fb114795fbe1230
Kamal <kamalfolarin@gmail.com>
2023-02-17 17:15:28 -0800

    if else

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * description - printing combination of numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 8; b++)
+		{
+			for (c = 0; c <= 9; c++)
+			{
+				for (d = 1; d<= 9; d++)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(' ');
+					putchar(c + '0');
+					putchar(d + '0');
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+		putchar('\n');
+		return (0);
+}
