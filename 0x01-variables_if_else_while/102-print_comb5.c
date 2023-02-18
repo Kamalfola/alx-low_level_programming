657c3897468ff561c45ddea8085898fd7eaaba84
Kamal <kamalfolarin@gmail.com>
2023-02-17 17:29:10 -0800

    if else

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,13 +11,14 @@ int main(void)
 	int b;
 	int c;
 	int d;
+
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 8; b++)
 		{
 			for (c = 0; c <= 9; c++)
 			{
-				for (d = 1; d<= 9; d++)
+				for (d = 1; d <= 9; d++)
 				{
 					putchar(a + '0');
 					putchar(b + '0');
